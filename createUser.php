d3be163fd5ec329cf8009c92e51f97ab53cd443c
lenahmbatha <lenahmbatha5@gmail.com>
2023-11-04 13:08:20 +0300

    crudPHP

--- createUser.php
+++ createUser.php
@@ -18,16 +18,18 @@ if(isset($_POST['submit'])){
 }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crud Operations</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" >
 </head>
 <body>
-    <div class="container">
-        <button type=" Add users" class="btn btn-primary  my-5" > Add Users</button>
+    <div class="container my-5">
+        <button type=" Add users" class="btn btn-primary" > Add Users</button>
         <form method="post">
             <div class=" form-group">
                 <label>name</label>
